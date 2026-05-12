062e6261a18a8a192f15d4d11b49932a843d4c01
Niek Neve <d317593@edu.curio.nl>
2026-05-12 12:39:15 +0200

    start choice.php

---
+++ choice.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html lang="nl">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Foto-kiosk</title>
+    <link rel="stylesheet" href="css/choice.css">
+</head>
+
+<body>
+
+    <header>
+
+    </header>
+    
+    <main>
+
+    <div class="container">
+        <div class="pic_vergeten">
+            <img src="https://placehold.co/400x200" alt="">
+            <button>Foto vergeten?</button>
+        </div>
+
+        <div class="pic_afdrukken">
+            <img src="https://placehold.co/400x200" alt="">
+            <button>Foto afdrukken?</button>
+        </div>
+    </div>
+
+    </main>
+
+    <footer>
+
+    </footer>
+
+</body>
+</html>
