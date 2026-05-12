61b1cd92f89b7bb391c208d9500cf45260c7ab0d
Bjorn076x <Bvklaveren@icloud.com>
2026-05-12 12:10:05 +0200

    `lol

---
+++ index.php
@@ -0,0 +1,42 @@
+<?php
+// index.php
+?>
+<!DOCTYPE html>
+<html lang="nl">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Foto Kiosk</title>
+
+    <link rel="stylesheet" href="css/index.css">
+</head>
+<body>
+
+<div class="wrapper">
+
+    <button class="kill-button" onclick="killKiosk()">
+        Klik op start
+    </button>
+
+    <button class="start-button" onclick="startKiosk()">
+        START
+    </button>
+
+</div>
+
+<script>
+    function startKiosk() {
+        alert('Foto kiosk gestart');
+
+        // window.location.href = 'camera.php';
+    }
+
+    function killKiosk() {
+        if(confirm('Weet je zeker dat je de kiosk wil laten starten?')) {
+            alert('Klik op start');
+        }
+    }
+</script>
+
+</body>
+</html>
