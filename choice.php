bde68aadf976befbdf42161c0144687586a763ac
Niek Neve <d317593@edu.curio.nl>
2026-05-26 12:17:02 +0200

    choice.php af

--- choice.php
+++ choice.php
@@ -14,12 +14,12 @@
 
         <div class="card">
             <img src="img/UGF2S0d5VFFuRy9CWGhGMHhKSGZKekdBTHEyNTY2TTZwNzFtWWNvZ0lKdkRhc05JQzRGbEFFZmtHdDZKODdBNER2TTBUYlI5Y2FWMUdsaWUvRkJYY3U5QjM5NXNFNjdxNDVhTHlOc1lUa0k9.jpg" alt="Foto vergeten">
-            <button class="action-btn">Foto vergeten?</button>
+            <a href="photoforgot.php"><button class="action-btn">Foto vergeten?</button></a>
         </div>
 
         <div class="card">
             <img src="img/cfe3a0ca6d6e89cd8ee075a5d75ec9f4.jpg" alt="Foto afdrukken">
-            <button class="action-btn">Foto afdrukken?</button>
+            <a href="photo.php"><button class="action-btn">Foto afdrukken?</button></a>
         </div>
 
     </div>
