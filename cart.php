4e562fa7f4e2f4a4293f79496f50ca29f00cb78d
Arnoud2208 <arnoudvoeten09@gmail.com>
2026-06-04 09:59:45 +0200

    ehe

--- cart.php
+++ cart.php
@@ -143,12 +143,7 @@ foreach ($cart as $item) {
                 <span>Totaal</span>
                 <span id="grand-total">€<?= number_format($total, 2, ',', '.') ?></span>
             </div>
-<<<<<<< Updated upstream
-
             <a href="payment.php" class="btn-pay">Betalen →</a>
-=======
-            <a href="betalen.php" class="btn-pay">Betalen →</a>
->>>>>>> Stashed changes
             <a href="photo.php" class="continue-link">← Verder winkelen</a>
         </div>
 
